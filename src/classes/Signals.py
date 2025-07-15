1af686819963f5f883d3dc28a1cf625eedb3f69f
Za9-118 <mypipe.email@gmail.com>
2025-07-15 01:44:55 +0100

    Add comments to the signals

--- src/classes/Signals.py
+++ src/classes/Signals.py
@@ -1,6 +1,11 @@
 data = {}
 
 class Signal:
+    """
+    A Signal object. This is used to communicate data to other Nodes in a Scene.
+    Pass an ID and NID into this class and everything's good to go.
+    (An NID is an unique number for each Signal, which, most of the time, is the amount of signals initialized)
+    """
     def __init__(self, id, nid):
         global data
         self.id    = id
@@ -8,20 +13,33 @@ class Signal:
         self.nid   = nid
     
     def fire(self, *args):
+        """Fire!!"""
         global data
         data[self.nid]["fired"] = 1
         data[self.nid]["data"]  = args
+    
+    def discard(self):
+        """Die"""
+        del data[self.nid]
+        del self.id
+        del self.nid
+        del self
 
 class SignalHandler:
+    """
+    This class handles all signals in the game. It is recommended to use this to make Signals instead of the Signal class.
+    """
     def __init__(self):
         self.signals = {}
     
     def add_signal(self, id):
+        """Add a signal to the list. Recommended instead of using Signal class. Pass an ID of any kind; String, Int, Float, etc..."""
         nid=len(self.signals)
         self.signals[nid] = Signal(id,nid)
         return self.signals[nid]
     
     def get(self):
+        """Get all the fired signals in the handler and empty the list"""
         global data
         fired_signals = []
         for i in self.signals:
@@ -29,4 +47,5 @@ class SignalHandler:
             fired,data=signal["fired"],signal["data"]
             if fired:
                 fired_signals.append((i,data))
+            signal["fired"] = 0
         return fired_signals
